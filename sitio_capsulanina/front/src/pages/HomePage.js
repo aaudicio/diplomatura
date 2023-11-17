f2de43325c563da55a0b85d62acd782016137263
aaudicio <aaudicio@frba.utn.edu.ar>
2023-11-17 14:36:26 -0300

    cambios generales

---
+++ sitio_capsulanina/front/src/pages/HomePage.js
@@ -0,0 +1,23 @@
+//import React from 'react';
+
+const HomePage = (props) => {
+    return (
+        <main className="holder"> 
+            <div className="homeimg">
+                <img src="images/uñas apaisada.jpeg" alt="logo nina" />
+            </div>
+            <div className="columnas">
+                <div className="bienvenidos">
+                    <h2>bienvenidos</h2>
+                    <p>Lorem </p>
+                    <p>Lorem </p>
+                </div>
+                    
+            </div>
+
+        </main>
+        
+    );
+}
+
+export default HomePage;
