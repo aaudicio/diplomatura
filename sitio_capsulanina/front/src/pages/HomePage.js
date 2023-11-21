1cc4e03365f0778d760dfd382398739ad8cb203a
Araceli Audicio <aaudicio@frba.utn.edu.ar>
2023-11-21 10:30:44 -0300

    armando html en react

--- sitio_capsulanina/front/src/pages/HomePage.js
+++ sitio_capsulanina/front/src/pages/HomePage.js
@@ -13,7 +13,11 @@ const HomePage = (props) => {
             <div className="columnas">
                 <div className="bienvenidos">
                     <h2>bienvenidos</h2>
-                     
+                    <p className="parrafo_bienvenido">Como sea que hayas llegado, te recibo con los brazos abiertos!</p>
+                    <p className="parrafo_bienvenido">Esta capsula fue armada con mucho esfuerzo y amor y quiero que la disfruten tanto como yo.</p>
+                    <p className="parrafo_bienvenido">En mi estética van a encontrar un lugar cómodo, donde se van a poder dedicar tiempo a ustedes.</p>  
+                    <p className="parrafo_bienvenido">Vernos y sentirnos bien depende de nosotros! </p>         
+                    <div className="ninago"><img src="images/nina2 sin fondo.png" width="150" alt="Capsula Nina2"/></div>
                 </div>
                     
             </div>
