d2be37f6b1b68e2948ebe29c2d0f966161877018
aaudicio <aaudicio@frba.utn.edu.ar>
2023-11-30 17:14:18 -0300

    Trabajo Final

--- sitio_capsulanina/back/app.js
+++ sitio_capsulanina/back/app.js
@@ -3,10 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fileUpload = require('express-fileupload');
+var cors = require('cors');
+
+require('dotenv').config();
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login')
+var adminRouter = require('./routes/admin/servicios');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -20,9 +27,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret:'bedrftsgbtb',
+  cookie: {maxAge: null },
+  resave: false,
+  saveUninitialized: true
+}))
+
+secured = async (req, res, next) => {
+  try {
+    console.log(req.session.id_usuario);
+    if (req.session.id_usuario) {
+      next();
+    } else {
+      res.redirect('/admin/login');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+app.use(fileUpload({
+  useTempFiles : true,
+  tempFileDir:  '/tmp'
+}));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/admin/login', loginRouter)
+app.use('/admin/login', loginRouter);
+app.use('/admin/servicios', secured, adminRouter);
+app.use('/api', cors(), apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,5 +73,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use(express.static(path.join(__dirname, '/public')));
+
 
 module.exports = app;
